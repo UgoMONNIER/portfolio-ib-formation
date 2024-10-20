518177305411e336331b17ec0ccc09e0ada08116
UgoMONNIER <ugomonnier@live.fr>
2024-10-20 21:29:16 +0200

    refacto js

--- js/index.js
+++ js/index.js
@@ -13,38 +13,5 @@ animateOnScrollElements.forEach((element) => {
 };
 
 
-
-
-const projetsLink = document.getElementById("projetsDropdown");
-const togglerIcon = document.querySelector(".navbar-collapse");
-const dropdownMenu = document.querySelector(".dropdown-menu");
-
-togglerIcon.addEventListener("click", function (event) {
-  const isOpen = togglerIcon.classList.contains("show");
-  console.log(isOpen);
-});
-
-projetsLink.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  const isOpen = dropdownMenu.classList.contains("show");
-
-  document.querySelectorAll(".dropdown-menu").forEach((menu) => {
-    menu.classList.remove("show");
-  });
-
-  if (!isOpen) {
-    dropdownMenu.classList.add("show");
-  } else {
-    window.location.href = "./projets/index.html";
-  }
-});
-
-document.addEventListener("click", (event) => {
-  if (!event.target.closest(".nav-item")) {
-    dropdownMenu.classList.remove("show");
-  }
-});
-
-window.addEventListener("scroll", animateElements);
 window.addEventListener("load", animateElements);
+window.addEventListener("scroll", animateElements);
